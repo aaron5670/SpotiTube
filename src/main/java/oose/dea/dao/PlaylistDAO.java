dd2a025c35abcc76f55acb04ee6f9f52550b8d34
Aaron van den Berg <a.vdberg98@gmail.com>
2020-03-10 21:25:02 +0100

    :sparkles: Added PlaylistDAO DAO

---
+++ src/main/java/oose/dea/dao/PlaylistDAO.java
@@ -0,0 +1,23 @@
+package oose.dea.dao;
+
+import oose.dea.dto.PlaylistsDTO;
+import javax.annotation.Resource;
+import javax.enterprise.inject.Default;
+import javax.sql.DataSource;
+
+@Default
+public class PlaylistDAO implements IPlaylistDAO {
+
+    @Resource(name = "jdbc/spotitubeMySQL")
+    DataSource dataSource;
+
+    @Override
+    public PlaylistsDTO getAllPlaylists(String token) {
+        //ToDo:
+        //  First verify if the token is real.
+        //  -- First create a token database table with a relation to users.
+        //  -- Need a new service class to check if this token exists in the database.
+        //  -- -- Need a TokenDAO too properly...
+        return null;
+    }
+}

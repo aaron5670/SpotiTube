eeecab113402fa93301b30780c348b1e31896e37
Aaron van den Berg <a.vdberg98@gmail.com>
2020-03-11 20:40:35 +0100

    :sparkles: Added TokenService

---
+++ src/main/java/oose/dea/service/TokenService.java
@@ -0,0 +1,19 @@
+package oose.dea.service;
+
+import oose.dea.dao.ITokenDAO;
+
+import javax.inject.Inject;
+
+public class TokenService {
+
+    private ITokenDAO iTokenDAO;
+
+    public boolean verifyToken(String token) {
+        return iTokenDAO.verifyToken(token);
+    }
+
+    @Inject
+    public void setiTokenDAO(ITokenDAO iTokenDAO) {
+        this.iTokenDAO = iTokenDAO;
+    }
+}

2ef149c205002ded266ed3d1874ce997ba1c0896
Aaron van den Berg <a.vdberg98@gmail.com>
2020-03-17 10:36:04 +0100

    :recycle: Refactored deleteAPlaylist DAO

--- src/main/java/oose/dea/dao/PlaylistDAO.java
+++ src/main/java/oose/dea/dao/PlaylistDAO.java
@@ -68,48 +68,18 @@ public class PlaylistDAO implements IPlaylistDAO {
     @Override
     public PlaylistsDTO deleteAPlaylist(int id, String token) {
         if (tokenService.verifyToken(token)) {
-            ArrayList<PlaylistDTO> playlists = new ArrayList<>();
-
             try (Connection connection = dataSource.getConnection()) {
                 String sql = "DELETE FROM playlists WHERE id = ?";
                 PreparedStatement preparedStatement = connection.prepareStatement(sql);
                 preparedStatement.setInt(1, id);
                 preparedStatement.executeUpdate();
 
-                sql = "SELECT p.id, p.name, p.owner, t.token FROM playlists p INNER JOIN users u on p.owner = u.username LEFT OUTER JOIN tokens t on u.username = t.username";
-                preparedStatement = connection.prepareStatement(sql);
-                ResultSet resultSet = preparedStatement.executeQuery();
-
-                while (resultSet.next()) {
-                    PlaylistDTO playlist = new PlaylistDTO();
-                    playlist.setId(resultSet.getInt("p.id"));
-                    playlist.setName(resultSet.getString("p.name"));
-
-                    if (resultSet.getString("t.token") != null && resultSet.getString("t.token").equals(token)) {
-                        playlist.setOwner(true);
-                    } else {
-                        playlist.setOwner(false);
-                    }
-
-                    //ToDo:
-                    //  Add tracks
-
-                    playlists.add(playlist);
-                }
-
-                //ToDo:
-                //  Calculate total length in seconds
-                playlistsDTO.setLength(100);
-
-                playlistsDTO.setPlaylists(playlists);
-
-                return playlistsDTO;
+                return getAllPlaylists(token);
             } catch (SQLException e) {
                 e.printStackTrace();
                 return null;
             }
         }
-
         return null;
     }
 

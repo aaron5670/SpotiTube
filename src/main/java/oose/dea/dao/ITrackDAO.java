a95cbc8509776f566e90bb8fda6c6bc5d8a44739
Aaron van den Berg <a.vdberg98@gmail.com>
2020-03-17 12:38:17 +0100

    :sparkles: Created ITrackDAO interface

---
+++ src/main/java/oose/dea/dao/ITrackDAO.java
@@ -0,0 +1,41 @@
+package oose.dea.dao;
+
+import oose.dea.dto.PlaylistDTO;
+import oose.dea.dto.PlaylistsDTO;
+import oose.dea.dto.TrackDTO;
+
+public interface ITrackDAO {
+
+    /**
+     *
+     * @param forPlaylist valid playlist
+     * @param token valid user token
+     * @return a response containing the complete list of available tracks
+     * @link https://github.com/aaron5670/SpotiTube#get-all-tracks
+     */
+    TrackDTO getAllTracks(String forPlaylist, String token);
+
+    /**
+     *
+     * @param token valid user token
+     * @return a response containing the complete list of tracks for the given Playlist
+     * @link https://github.com/aaron5670/SpotiTube#get-all-tracks-that-belong-to-a-playlist
+     */
+    TrackDTO getTracksFromPlaylist(String token);
+
+    /**
+     *
+     * @param token valid user token
+     * @return a response containing the complete and modified list of tracks
+     * @link https://github.com/aaron5670/SpotiTube#remove-a-track-from-a-playlist
+     */
+    TrackDTO removeTrackFromPlaylist(String token);
+
+    /**
+     *
+     * @param token valid user token
+     * @return a response containing the complete list of tracks for the given
+     * @link https://github.com/aaron5670/SpotiTube#add-a-track-to-a-playlist
+     */
+    TrackDTO addTrackToPlaylist(String token);
+}
